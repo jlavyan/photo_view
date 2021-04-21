c9f118f28ff7c7e8777efcf0010dc9ae59796365
Grigori <jlavyan@gmail.com>
2021-04-21 19:52:42 +0400

    ADD log

--- lib/src/core/gesture_scale_bloc.dart
+++ lib/src/core/gesture_scale_bloc.dart
@@ -23,11 +23,16 @@ class _GestureScaleBlocState extends State<GestureScaleBloc> {
   bool get _isEnabled => pointers > 1;
   @override
   Widget build(BuildContext context) {
+    print('state change');
     return Listener(
-        onPointerDown: (event) =>
-            pointers++, // When user will, say, scale some image(two pointers/fingers), <onPointerDown> callback will be called two times, and will increase <pointers> variable to two.
-        onPointerUp: (event) =>
-            pointers--, // This callback will be called when any user touch will be canceled. You can instead callback [(event) => pointers--] use [(event) => pointers = 0] to ensure that <pointers> variable is reseted.
+        onPointerDown: (event) {
+            pointers++;
+            print(pointers); // When user will, say, scale some image(two pointers/fingers), <onPointerDown> callback will be called two times, and will increase <pointers> variable to two
+        }
+        onPointerUp: (event) {
+            pointers--; // This callback will be called when any user touch will be canceled. You can instead callback [(event) => pointers--] use [(event) => pointers = 0] to ensure that <pointers> variable is resete
+            print(pointers);
+        }
         child: GestureDetector(
             behavior: HitTestBehavior.opaque,
             onScaleStart: _isEnabled ? widget.onScaleStart : null,
